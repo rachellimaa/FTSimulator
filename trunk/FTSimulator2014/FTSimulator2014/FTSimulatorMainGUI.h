a14b7d224bdf13f0da93ac87484447c5ceadda4f
rachellimaa@gmail.com <rachellimaa@gmail.com@d3905931-30ec-db61-1e68-540b5685ca95>
2014-11-12 14:19:14 +0000

    fiz a primeira tela

--- trunk/FTSimulator2014/FTSimulator2014/FTSimulatorMainGUI.h
+++ trunk/FTSimulator2014/FTSimulator2014/FTSimulatorMainGUI.h
@@ -34,6 +34,15 @@ namespace FTSimulator2014 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::LinkLabel^  linkLabel1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
 
 	private:
 		/// <summary>
@@ -48,12 +57,126 @@ namespace FTSimulator2014 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"FTSimulatorMainGUI";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->linkLabel1 = (gcnew System::Windows::Forms::LinkLabel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(110, 230);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(100, 32);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L" > FFT";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->Location = System::Drawing::Point(508, 229);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(100, 33);
+			this->button2->TabIndex = 3;
+			this->button2->Text = L"> QFT";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// linkLabel1
+			// 
+			this->linkLabel1->AutoSize = true;
+			this->linkLabel1->Location = System::Drawing::Point(257, 361);
+			this->linkLabel1->Name = L"linkLabel1";
+			this->linkLabel1->Size = System::Drawing::Size(193, 13);
+			this->linkLabel1->TabIndex = 4;
+			this->linkLabel1->TabStop = true;
+			this->linkLabel1->Text = L"https://code.google.com/p/ftsimulator/";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(276, 338);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(147, 13);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"Visit our project homepage at:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(107, 51);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(487, 17);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"FTSimulator is an open-source tool for Classical and Quantum Fourier Transforms.";
+			// 
+			// textBox1
+			// 
+			this->textBox1->BackColor = System::Drawing::SystemColors::ScrollBar;
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox1->Location = System::Drawing::Point(260, 2);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(182, 36);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->Text = L"FTSimulator";
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &FTSimulatorMainGUI::textBox1_TextChanged);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(50, 122);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(377, 20);
+			this->label3->TabIndex = 7;
+			this->label3->Text = L"There are two types of Fourier Transforms available. ";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(50, 156);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(195, 20);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"Choose your option below:";
+			// 
+			// FTSimulatorMainGUI
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::ScrollBar;
+			this->ClientSize = System::Drawing::Size(703, 383);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->linkLabel1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"FTSimulatorMainGUI";
+			this->Text = L"FTSimulatorMainGUI";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+        
+	}
+};
 }
