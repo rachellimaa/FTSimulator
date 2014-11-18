6fca0986e92f7e32d7db9b942a3837e64ae51047
rachellimaa <rachellimaa@gmail.com>
2014-11-18 13:49:10 +0000

    Tá começando a ganhar forma rss

---
+++ FTSimulator2014/FTSimulator2014/pag4.h
@@ -0,0 +1,197 @@
+#pragma once
+
+#include "pag5.h"
+
+namespace FTSimulator2014 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Sumário para pag4
+	/// </summary>
+	public ref class pag4 : public System::Windows::Forms::Form
+	{
+	public:
+		pag4(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Adicione o código do construtor aqui
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		~pag4()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^  label4;
+	protected:
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::LinkLabel^  linkLabel1;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Button^  button2;
+
+	private:
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necessário para suporte do Designer - não modifique
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->linkLabel1 = (gcnew System::Windows::Forms::LinkLabel());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(109, 171);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(87, 20);
+			this->label4->TabIndex = 32;
+			this->label4->Text = L"Input Files:";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(238, 90);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(230, 20);
+			this->label3->TabIndex = 31;
+			this->label3->Text = L"Quantum Fourier Transform";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(110, 41);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(487, 17);
+			this->label2->TabIndex = 30;
+			this->label2->Text = L"FTSimulator is an open-source tool for Classical and Quantum Fourier Transforms.";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(278, 320);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(147, 13);
+			this->label1->TabIndex = 29;
+			this->label1->Text = L"Visit our project homepage at:";
+			// 
+			// linkLabel1
+			// 
+			this->linkLabel1->AutoSize = true;
+			this->linkLabel1->Location = System::Drawing::Point(260, 340);
+			this->linkLabel1->Name = L"linkLabel1";
+			this->linkLabel1->Size = System::Drawing::Size(193, 13);
+			this->linkLabel1->TabIndex = 28;
+			this->linkLabel1->TabStop = true;
+			this->linkLabel1->Text = L"https://code.google.com/p/ftsimulator/";
+			this->linkLabel1->LinkClicked += gcnew System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &pag4::linkLabel1_LinkClicked);
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(548, 251);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(100, 32);
+			this->button1->TabIndex = 27;
+			this->button1->Text = L" > Proceed";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &pag4::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->BackColor = System::Drawing::SystemColors::ScrollBar;
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox1->Location = System::Drawing::Point(271, 2);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(182, 36);
+			this->textBox1->TabIndex = 26;
+			this->textBox1->Text = L"FTSimulator";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(214, 170);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(211, 20);
+			this->textBox2->TabIndex = 33;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(453, 159);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(89, 40);
+			this->button2->TabIndex = 34;
+			this->button2->Text = L"Browse Files";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// pag4
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::ScrollBar;
+			this->ClientSize = System::Drawing::Size(701, 362);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->linkLabel1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"pag4";
+			this->Text = L"pag4";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void linkLabel1_LinkClicked(System::Object^  sender, System::Windows::Forms::LinkLabelLinkClickedEventArgs^  e) {
+				 System::Diagnostics::Process::Start("https://code.google.com/p/ftsimulator/");
+	}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+			 pag5 ^ form = gcnew pag5;
+			 form->ShowDialog();
+}
+};
+}
